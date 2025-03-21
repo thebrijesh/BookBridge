f7452d85e019382549b5d62534368ab9488649ef
brijesh <ibrijeshshingala@gmail.com>
2025-03-21 14:27:58 +0530

    Initial commit

---
+++ src/main/java/com/brijesh/bookbridge/BookBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.brijesh.bookbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brijesh/bookbridge/BookBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brijesh.bookbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
